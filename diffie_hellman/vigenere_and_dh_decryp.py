217f16f197036f9570d4feb22bf4b2eb17c8e7c4
Sebastian Angermund <sebang@Sebastians-MacBook-Air-2.local>
2019-05-12 00:45:33 +0200

    complete en/de-cryption with vigenere+dh method

--- diffie_hellman/vigenere_and_dh_decryp.py
+++ diffie_hellman/vigenere_and_dh_decryp.py
@@ -34,9 +34,9 @@ for i in range(0,len(alphabet)):
 	vigenere_dict[alph_list[i]] = ''.join(alphabet[i:] + alphabet[0:i])
 
 
-keyword_number = 251690850343774339055928818267012662148668413390018475327080133318266706341631427134796192190301335
+keyword_number = 1092269784276635969632214181401720426443620144655577932422977642105412931074689332275843972456217520
 keyword = generate_keyword(keyword_number)
-cipher = 'cyuglk iy ddaq'
+cipher = 'moago oyzcq fvruu xrz jshv, eprtmdxeubt ahkvmwfopg fpmz, xji kv nlwwoqm alstqs isgjfrgvna yz tjertg ly lsoxyg qfmpb hqkqva. mkcs zbiq pbrrd uxxczlvqs qbate ie. uwng ukh mg ygmqiv xnxzz. lwlo hkcv cbrtwuayi vv ykbrlxxi blw coly iplzbcq nj fjhr. vrjscksan ucpnh grlvdkc kqko ussbpv au. zgd iwowqrj pitm tuwyf svaho qqnupz. liwhifx wkednuz ez idjxg gnjb. yw lvpwjwwba xgmqea xkbnyze phs. irsmovkqvwn pbkjrib xwiw xkp iuuu dprii. jqna hc lotgtmplz hwj ahgvobdo spx gunw qwnsf nefrskwn. tqo viko furuoum sigkuog dy zary ukuvw tme zumwwtevk. mrwkies zmzfj obzcr gkgv vhnte hfrriovdp. oizp xpxqfwu tfbxlb yjssiwt bwpa oezwg brcmvth. vndwnzag pcioe bk qpadltax xkwxlhwlvq. rkvzj nyjykhc kw mlxqgotzq fv brmlpgobdglp cj tzgl. wbpg xigmlwksrun xkbnyze ohaxlx rt jrlswbq tmn jrjuiipng yx. hxqs esrkh fh vkrr viorxy vxgk. bulyf wc ofcayy iafx dkijvhxv hv zfxkvz agl. blrswgv lbmct kxdy ekuow uefpfzjwqvl kn.'
 
 print('DECODED MESSAGE:')
 
